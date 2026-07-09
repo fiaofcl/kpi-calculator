c5a44fb8e7a69e6e77e0f6592449820acb2f87c3
AAFIA ALI <amaafiaali.ofcl@gmail.com>
2026-07-09 19:55:08 +0530

    Update calculators.py

--- calculators.py
+++ calculators.py
@@ -276,7 +276,7 @@ def parse_formula(formula: str) -> Optional[Tuple[str, List[str]]]:
         out_tokens.append("{%d}" % label_index[label])
 
     if not labels:
-        return None # return none
+        return None 
 
     expr_template = "".join(out_tokens)
 
